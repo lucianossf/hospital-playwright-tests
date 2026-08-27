af5b245942ce5f6fc944351bd771782c18431cb3
Luciano S <lucianosilveirasf@gmail.com>
2026-08-27 10:21:48 -0300

    test: add calendar provider filter

--- tests/calendar/calendar.spec.ts
+++ tests/calendar/calendar.spec.ts
@@ -3,3 +3,7 @@ import { test } from '../../fixtures/testFixtures';
 test('TC-104 | deve exibir agenda com visões diária semanal e mensal', async ({ dashboardPage }) => {
   await dashboardPage.expectCalendarAvailable();
 });
+
+test('TC-112 | deve disponibilizar filtro de profissionais na agenda', async ({ dashboardPage }) => {
+  await dashboardPage.expectCalendarProviderFilter();
+});
